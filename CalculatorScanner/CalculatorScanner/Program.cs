8fb86e163e1fe562c525df019a863ce9d065acd8
Craigory Coppola <craigorycoppola@gmail.com>
2019-09-26 12:13:04 -0400

    Read File Input, get queue<Char>

---
+++ CalculatorScanner/CalculatorScanner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculatorScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner s = new Scanner(@"..\..\..\sample.calc");
+            s.GetTokens();
+        }
+    }
+}
---
+++ CalculatorScanner/CalculatorScanner/Scanner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CalculatorScanner
+{
+    
+    class Scanner
+    {
+        string filePath;
+        public Scanner(string FilePath)
+        {
+            filePath = FilePath;
+        }
+
+        public List<string> GetTokens()
+        {
+            List<string> tokens = new List<string>();
+            using (FileStream f = new FileStream(filePath, FileMode.Open))
+            {
+                StreamReader reader = new StreamReader(f);
+                Queue<char> queue = new Queue<char>(reader.ReadToEnd());
+                foreach (var item in queue)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            return tokens;
+        }
+
+    }
+}
